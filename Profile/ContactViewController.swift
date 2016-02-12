1a8d2de095f0f6977f46edef6ae23a71bf9e11f1
Ian Hirschfeld <ianh7976@gmail.com>
2016-02-12 15:45:06 -0800

    Adds photography credits for David.

--- Profile/ContactViewController.swift
+++ Profile/ContactViewController.swift
@@ -14,12 +14,13 @@ class ContactViewController: PROViewController {
   // PROPERTIES
   // ==================================================
 
-  @IBOutlet weak var companyLabel: UILabel!
   @IBOutlet weak var addressLabel: UILabel!
-  @IBOutlet weak var twitterButton: UIButton!
+  @IBOutlet weak var companyLabel: UILabel!
+  @IBOutlet weak var contactUsLabel: UILabel!
   @IBOutlet weak var emailButton: UIButton!
   @IBOutlet weak var phoneButton: UIButton!
-  @IBOutlet weak var contactUsLabel: UILabel!
+  @IBOutlet weak var photoCreditsButton: UIButton!
+  @IBOutlet weak var twitterButton: UIButton!
 
   // ==================================================
   // METHODS
@@ -28,35 +29,43 @@ class ContactViewController: PROViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    twitterButton.setImage(twitterButton.imageView?.image?.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
     emailButton.setImage(emailButton.imageView?.image?.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
     phoneButton.setImage(phoneButton.imageView?.image?.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
+    twitterButton.setImage(twitterButton.imageView?.image?.imageWithRenderingMode(.AlwaysTemplate), forState: .Normal)
   }
 
   override func updateColors() {
     view.backgroundColor = UIColor.appInvertedPrimaryBackgroundColor()
-    companyLabel.textColor = UIColor.appInvertedPrimaryTextColor()
     addressLabel.textColor = UIColor.appInvertedSecondaryTextColor()
-    twitterButton.tintColor = UIColor.appInvertedPrimaryTextColor()
-    twitterButton.setTitleColor(UIColor.appInvertedPrimaryTextColor(), forState: .Normal)
+    companyLabel.textColor = UIColor.appInvertedPrimaryTextColor()
+    contactUsLabel.textColor = UIColor.appInvertedSecondaryTextColor()
     emailButton.tintColor = UIColor.appInvertedPrimaryTextColor()
     emailButton.setTitleColor(UIColor.appInvertedPrimaryTextColor(), forState: .Normal)
     phoneButton.tintColor = UIColor.appInvertedPrimaryTextColor()
     phoneButton.setTitleColor(UIColor.appInvertedPrimaryTextColor(), forState: .Normal)
-    contactUsLabel.textColor = UIColor.appInvertedSecondaryTextColor()
+    photoCreditsButton.setTitleColor(UIColor.appInvertedSecondaryTextColor(), forState: .Normal)
+    twitterButton.tintColor = UIColor.appInvertedPrimaryTextColor()
+    twitterButton.setTitleColor(UIColor.appInvertedPrimaryTextColor(), forState: .Normal)
   }
 
-  @IBAction func twitterTapped(sender: UIButton) {
-    guard let url = NSURL(string: "https://twitter.com/soapcollective") else { return }
-    UIApplication.sharedApplication().openURL(url)
-  }
+
 
   @IBAction func emailTapped(sender: UIButton) {
     tryToEmail("info@thesoapcollective.com", subject: "Hey Soap!")
   }
 
+  @IBAction func photoCreditsTapped(sender: UIButton) {
+    guard let url = NSURL(string: "http://www.daviddatnguyen.com") else { return }
+    UIApplication.sharedApplication().openURL(url)
+  }
+
   @IBAction func phoneTapped(sender: UIButton) {
     askToCall("1-857-203-1004", name: "us")
+  }
+
+  @IBAction func twitterTapped(sender: UIButton) {
+    guard let url = NSURL(string: "https://twitter.com/soapcollective") else { return }
+    UIApplication.sharedApplication().openURL(url)
   }
 
   // ==================================================
