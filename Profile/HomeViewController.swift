164bf590cf330a33ad5c2fe69ae65272a257e8a2
Ian Hirschfeld <ianh7976@gmail.com>
2016-02-13 16:08:44 -0800

    Adds home screen description that's revealed on logo tap.

--- Profile/HomeViewController.swift
+++ Profile/HomeViewController.swift
@@ -15,6 +15,8 @@ class HomeViewController: PROViewController {
   // ==================================================
 
   @IBOutlet weak var bottomGradientView: LinearGradientView!
+  @IBOutlet weak var descriptionContainerView: ParallaxView!
+  @IBOutlet weak var descriptionLabel: UILabel!
   @IBOutlet weak var radialGradientContainerView: UIView!
   @IBOutlet weak var radialGradientView: RadialGradientView!
   @IBOutlet weak var indexIconContainerView: UIView!
@@ -29,7 +31,11 @@ class HomeViewController: PROViewController {
   @IBOutlet weak var radialGradientTopConstraint: NSLayoutConstraint!
   @IBOutlet weak var radialGradientTrailingConstraint: NSLayoutConstraint!
 
+  let DayDescription = "The Soap Collective is a multi-media creative agency. We focus on building high-end interactive experiences that connect people and inspire emotion."
+  let NighDescription = "Looks like you found Side B... Scroll through in this mode to get behind the scenes trivia about our projects and team."
+
   var index = 0
+  var isDescriptionShowing = false
 
   // ==================================================
   // METHODS
@@ -38,11 +44,12 @@ class HomeViewController: PROViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    descriptionContainerView.layer.borderWidth = 1
     logoImageView.image = logoImageView.image?.imageWithRenderingMode(.AlwaysTemplate)
     indexIconImageView.image = indexIconImageView.image?.imageWithRenderingMode(.AlwaysTemplate)
     mailIconImageView.image = mailIconImageView.image?.imageWithRenderingMode(.AlwaysTemplate)
 
-    setupGestures()
+    setModeDependentAttributes()
   }
 
   override func viewDidLayoutSubviews() {
@@ -56,9 +63,16 @@ class HomeViewController: PROViewController {
     }
   }
 
+  override func profileModeChanged(notification: NSNotification) {
+    super.profileModeChanged(notification)
+    setModeDependentAttributes()
+  }
+
   override func updateColors() {
     bottomGradientView.fromColor = UIColor.appPrimaryBackgroundColor().colorWithAlphaComponent(0)
     bottomGradientView.toColor = UIColor.appPrimaryBackgroundColor()
+    descriptionLabel.textColor = UIColor.appPrimaryTextColor()
+    descriptionContainerView.layer.borderColor = UIColor.appPrimaryTextColor().colorWithAlphaComponent(0.75).CGColor
     indexIconImageView.tintColor = UIColor.appPrimaryTextColor()
     indexIconDottedBorderImageView.dotColor = UIColor.appPrimaryTextColor()
     logoImageView.tintColor = UIColor.appPrimaryTextColor()
@@ -71,13 +85,42 @@ class HomeViewController: PROViewController {
   }
 
   func addParallaxToViews() {
-    logoImageView.addParallax(Global.ParallaxOffset1)
     radialGradientView.addParallax(Global.ParallaxOffset2)
+    logoImageView.addParallax(Global.ParallaxOffset1)
+    descriptionContainerView.addParallax(Global.ParallaxOffset1)
   }
 
   func removeParallaxFromViews() {
     logoImageView.removeParallax()
     radialGradientView.removeParallax()
+  }
+
+  func setModeDependentAttributes() {
+    descriptionLabel.text = Global.mode == .Light ? DayDescription : NighDescription
+  }
+
+  func toggleDescription() {
+    if isDescriptionShowing {
+      UIView.animateWithDuration(0.3, animations: { () -> Void in
+        self.descriptionContainerView.alpha = 0
+      }, completion: { (completed) -> Void in
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+          self.logoImageView.alpha = 1
+        }, completion: { (completed) -> Void in
+          self.isDescriptionShowing = false
+        })
+      })
+    } else {
+      UIView.animateWithDuration(0.3, animations: { () -> Void in
+        self.logoImageView.alpha = 0
+      }, completion: { (completed) -> Void in
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+          self.descriptionContainerView.alpha = 1
+        }, completion: { (completed) -> Void in
+          self.isDescriptionShowing = true
+        })
+      })
+    }
   }
 
   // ==================================================
@@ -88,8 +131,14 @@ class HomeViewController: PROViewController {
     let contactTapGesture = UITapGestureRecognizer(target: self, action: "contactTapped:")
     mailIconContainerView.addGestureRecognizer(contactTapGesture)
 
+    let descriptionTapGesture = UITapGestureRecognizer(target: self, action: "descriptionTapped:")
+    descriptionContainerView.addGestureRecognizer(descriptionTapGesture)
+
     let indexTapGesture = UITapGestureRecognizer(target: self, action: "indexTapped:")
     indexIconContainerView.addGestureRecognizer(indexTapGesture)
+
+    let logoTapGesture = UITapGestureRecognizer(target: self, action: "logoTapped:")
+    logoImageView.addGestureRecognizer(logoTapGesture)
   }
 
   func contactTapped(notification: NSNotification) {
@@ -97,9 +146,17 @@ class HomeViewController: PROViewController {
     NSNotificationCenter.defaultCenter().postNotificationName(notificationName, object: nil)
   }
 
+  func descriptionTapped(notification: NSNotification) {
+    toggleDescription()
+  }
+
   func indexTapped(notification: NSNotification) {
     let notificationName = Global.isIndexOpen ? Global.CloseIndexNotification : Global.OpenIndexNotification
     NSNotificationCenter.defaultCenter().postNotificationName(notificationName, object: nil)
+  }
+
+  func logoTapped(notification: NSNotification) {
+    toggleDescription()
   }
 
   // ==================================================
@@ -119,6 +176,7 @@ class HomeViewController: PROViewController {
 
   func appBooted(notification: NSNotification) {
     addParallaxToViews()
+    setupGestures()
   }
 
   func contactPanning(notification: NSNotification) {
